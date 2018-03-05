e16a4da49603af1da34190f2cc0764e2123615a5
Joshua Hutchinson <jthutch94@gmail.com>
2018-03-05 15:47:59 -0500

    chellenge-1

--- server.js
+++ server.js
@@ -2,7 +2,22 @@
 
 // TEMP: Simple In-Memory Database
 const data = require('./db/notes');
-
 console.log('hello world!');
 
 // INSERT EXPRESS APP CODE HERE...
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.listen(8080, function(){
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
+
+app.get('/api/notes', (req, res)=> {
+  res.json(data);
+});
+
+app.get('/api/notes/:id', (req, res)=>{
+  res.json(data.find(item=> item.id === Number(req.params.id)));
+});
